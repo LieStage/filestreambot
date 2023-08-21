2da38ea5d102dfbcae895fd7c0d96c45a963209b
LieStage <118051769+LieStage@users.noreply.github.com>
2023-08-21 15:38:57 +0530

    Update stream.py

--- Adarsh/bot/plugins/stream.py
+++ Adarsh/bot/plugins/stream.py
@@ -16,9 +16,9 @@ shortz = shortzy.Shortzy(Var.SHORTENER_API, "dtglinks.in")
 async def get_shortlink(online_link):
     if Var.SHORTENER_API:
         if Var.LONG_DROPLINK_URL == "True" or Var.LONG_DROPLINK_URL is True:
-            return await shortz.get_quick_link(online_link)
+            return  shortz.get_quick_link(online_link)
         else:
-            return await shortz.convert(online_link, silently_fail=False)
+            return shortz.convert(online_link, silently_fail=False)
     return link
 
 @StreamBot.on_message(filters.private & (filters.document | filters.video | filters.audio) & ~filters.edited, group=4)
